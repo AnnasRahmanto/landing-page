97f842fcfbae648d1fed44fde8c92f4797785ea1
Annas <annasrahmanto@gmail.com>
2024-08-07 13:43:42 +0700

    add : page about Us, contact, gallery, services

--- src/pages/about Us/index.js
+++ src/pages/about Us/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import Img01 from "../../assets/img/img-01.jpg";
+import Team01 from "../../assets/img/team-01.jpg";
+import Team02 from "../../assets/img/team-02.jpg";
+import Team03 from "../../assets/img/team-03.jpg";
+import Team04 from "../../assets/img/team-04.jpg";
+import Team05 from "../../assets/img/team-05.jpg";
+import Team06 from "../../assets/img/team-06.jpg";
 
 const aboutUs = () => {
+  const sectionImg = [
+    { id: 1, name: "Frank Star", job: "Boss", img: Team01 },
+    { id: 2, name: "Jane Naismith", job: "Graphic Designer", img: Team02 },
+    { id: 3, name: "Mark Stoner", job: "Web Designer", img: Team03 },
+    { id: 4, name: "Steve Carpenter", job: "Web Developer", img: Team04 },
+    { id: 5, name: "Steff Kingston", job: "Account Manager", img: Team05 },
+    { id: 7, name: "John Sandman", job: "Account Manager", img: Team06 },
+  ];
+
   return (
     <div>
       {/*-- TOP SECTION --*/}
@@ -46,6 +61,70 @@ const aboutUs = () => {
 
         {/*-- Image-->*/}
         <img className="l-6 hide-s hide-m" src={Img01} alt="pict-alt" />
+      </section>
+
+      {/*-- SECTION 2 --*/}
+      <section className="grid margin margin-bottom padding-2x background-primary">
+        <div className="s-12 m-6 l-3">
+          <span className="timer text-size-50 text-white text-strong">
+            1500
+          </span>
+          <h3 className="text-uppercase text-strong">Ultricies eros sociis</h3>
+          <p className="text-size-16 margin-m-bottom-20">
+            Eodem modo typi nonummy maecenas ultricies eros sociis hendrerit
+            dictum.
+          </p>
+        </div>
+
+        <div className="s-12 m-6 l-3">
+          <span className="timer text-size-50 text-white text-strong">95</span>{" "}
+          <span className="text-size-50 text-white text-strong">%</span>
+          <h3 className="text-uppercase text-strong">
+            Magna condimentum suspendisse
+          </h3>
+          <p className="text-size-16 margin-m-bottom-20">
+            Duis autem libero ullam magna condimentum suspendisse pellentesque.
+          </p>
+        </div>
+
+        <div className="s-12 m-6 l-3">
+          <span className="timer text-size-50 text-white text-strong">287</span>
+          <h3 className="text-uppercase text-strong">
+            Consequat phasellus orci
+          </h3>
+          <p className="text-size-16 margin-m-bottom-20">
+            Mirum est notare dolor consequat phasellus orci pellentesque
+            hendrerit.
+          </p>
+        </div>
+
+        <div className="s-12 m-6 l-3">
+          <span className="timer text-size-50 text-white text-strong">7</span>{" "}
+          <span className="text-size-50 text-white text-strong">milion</span>
+          <h3 className="text-uppercase text-strong">Augue et venenatis</h3>
+          <p className="text-size-16 margin-m-bottom-20">
+            Nam liber tempor erat augue et venenatis lorem ipsum dolor si amet.
+          </p>
+        </div>
+      </section>
+      {/*-- SECTION 3 --*/}
+      <section className="grid margin">
+        <h2 className="s-12 text-white text-thin text-size-30 text-white text-uppercase margin-top-bottom-20 center text-center">
+          Our <b>Dream Team</b>
+        </h2>
+        {sectionImg.map((data) => {
+          return (
+            <div className="s-12 m-6 l-2 margin-bottom" key={data.id}>
+              <img src={data.img} alt="pict-alt" />
+              <h4 className="background-primary padding text-strong">
+                {data.name}
+              </h4>
+              <p className="margin-bottom-10 text-primary text-uppercase">
+                {data.job}
+              </p>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,9 +1,48 @@
 import React from "react";
+import Thumb01 from "../../assets/img/portfolio/thumb-01.jpg";
+import Thumb02 from "../../assets/img/portfolio/thumb-02.jpg";
+import Thumb03 from "../../assets/img/portfolio/thumb-03.jpg";
+import Thumb04 from "../../assets/img/portfolio/thumb-04.jpg";
+import Thumb05 from "../../assets/img/portfolio/thumb-05.jpg";
+import Thumb06 from "../../assets/img/portfolio/thumb-06.jpg";
 
 const gallery = () => {
+  const Image = [
+    { name: "Thumb 01", img: Thumb01 },
+    { name: "Thumb 02", img: Thumb02 },
+    { name: "Thumb 03", img: Thumb03 },
+    { name: "Thumb 04", img: Thumb04 },
+    { name: "Thumb 05", img: Thumb05 },
+    { name: "Thumb 06", img: Thumb06 },
+  ];
+
   return (
     <div>
-      <h1>this in GALLERY page</h1>
+      {/*<!-- TOP SECTION -->*/}
+      <header className="grid">
+        <div className="s-12 padding-2x">
+          <h1 className="text-strong text-white text-center center text-size-60 text-uppercase margin-bottom-20">
+            Our References
+          </h1>
+          <p className="text-size-20 text-white text-center center text-uppercase">
+            Con nonummy sem integer interdum volutpat dis eget eligendi aliquip
+            dolorum magnam. Iriure duis autem vel
+            <br /> eum dolor in hendrerit in vulputate velit esse molestie
+            consequat.
+          </p>
+        </div>
+      </header>
+
+      <section className="grid margin">
+        {Image.map((item, index) => (
+          <img
+            key={index}
+            className="s-12 m-6 margin-bottom"
+            src={item.img}
+            alt={item.name}
+          />
+        ))}
+      </section>
     </div>
   );
 };
